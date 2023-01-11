d25250cf7f138acbc994d103d3e4a781676d5799
Kim Shin <kimshin@Kimui-MacBookAir-2.local>
2023-01-11 14:28:31 +0900

    2023/1/11

---
+++ BOJ_1240_노드사이의 거리.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+n, m = map(int, input().split())
+graph = [[] for i in range(n+1)]
+inp = []
+
+for i in range(n-1):
+    a, b, r = map(int, input().split())
+    # print(graph)
+    graph[a].append((b, r))
+    graph[b].append((a, r))
+
+for i in range(m):
+    inp.append(set(map(int, input().split())))
+
+
+def bfs(start, to):
+    queue = deque()
+    visited = [False] * (n+1)
+    visited[start] = True
+    d = 0
+    queue.append((start, 0))
+
+    while queue:
+        v, d = queue.popleft()
+        if v == to:
+            return d
+
+        for i, j in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append((i, j+d))
+
+
+for i, j in inp:
+    print(bfs(i, j))
---
+++ BOJ_1436_영화감독 숌.py
@@ -0,0 +1,11 @@
+x = int(input())
+
+n = 0
+count = 0
+
+while count != x:
+    if str(n).count("666") != 0:
+        count += 1
+    n += 1
+
+print(n - 1)
