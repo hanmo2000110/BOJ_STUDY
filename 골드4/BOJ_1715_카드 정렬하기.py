c8e969f9bae2c58def9f865e5b898a76d6b23e41
Kim Shin <kimshin@Kimui-MacBookAir-2.local>
2023-01-18 23:56:07 +0900

    문제 난이도 별로 분류 및 제출 미제출 태그 설정 하기로 결정

---
+++ 골드4/BOJ_1715_카드 정렬하기.py
@@ -0,0 +1,34 @@
+'''
+카드 정렬하기
+https://www.acmicpc.net/problem/1715
+자료 구조, 그리디 알고리즘, 우선순위 큐
+
+접근법 :
+정렬 후 a + b 그리고 결과값을 리스트의 앞부분에 어펜드 후 리스트가 끝날때까지
+만약 숫자가 하나만 입력된다면 두 리스트를 합칠 필요가 없으니 0번의 비교가 필요함
+
+항상 가장 작은 두개를 합쳐야 하기 때문에 매번 소팅해줘야 한다
+매번 정렬을 반복해야 한다면 heapq 사용하겠다
+'''
+
+from collections import deque
+from heapq import heappush, heappop
+
+n = int(input())
+l = []
+for i in range(n):
+    heappush(l, int(input()))
+
+if n == 1:
+    print(0)
+    exit()
+count = 0
+while True:
+    a = heappop(l)
+    b = heappop(l)
+    s = a + b
+    count += s
+    if len(l) == 0:
+        print(count)
+        break
+    heappush(l, s)
---
+++ 실버3/BOJ_1003_피보나치 함수.py
@@ -0,0 +1,33 @@
+'''
+피보나치 함수
+https://www.acmicpc.net/problem/1003
+다이나믹 프로그래밍
+
+접근법 :
+디피로 각 피보나치마다 0과 1이 호출된 횟수를 계산한다 
+
+점화식 :
+a0[0] = 1
+a0[1] = 0
+a0[i] = a0[i-1] + a0[i-2]
+
+a1[0] = 0
+a1[1] = 1
+a1[i] = a1[i-1] + a[i-2]
+'''
+
+t = int(input())
+l = [int(input()) for i in range(t)]
+
+a0 = [0] * 41
+a1 = [0] * 41
+a0[0] = 1
+a0[1] = 0
+a1[0] = 0
+a1[1] = 1
+for i in range(2, 41):
+    a0[i] = a0[i-1] + a0[i-2]
+    a1[i] = a1[i-1] + a1[i-2]
+
+for x in l:
+    print(a0[x], a1[x])
