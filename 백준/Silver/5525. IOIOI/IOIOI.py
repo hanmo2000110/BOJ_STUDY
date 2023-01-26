cea072426ae5dcdafd912d5326fbf5c7bb7bb2e3
Kim Shin <kimshin@Kimui-MacBookAir-2.local>
2023-01-26 22:50:31 +0900

    구조 바꿈

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,3 +1,17 @@
+# 50점 코드
+N = int(input())
+M = int(input())
+S = input()
+P = 'IO'*N + 'I'
+answer = 0
+
+for i in range(M - len(P)):
+    if S[i] == 'I':
+        if S[i:i+len(P)] == P:
+            answer += 1
+print(answer)
+# -------------------------------------------------------------
+# 100점 코드
 N = int(input())
 M = int(input())
 S = input()
