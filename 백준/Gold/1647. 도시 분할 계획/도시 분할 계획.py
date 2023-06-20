f7b90f06003ed1a55f405cbcb177eeab3691f1d0
hanmo2000110 <74346222+hanmo2000110@users.noreply.github.com>
2023-06-21 01:09:16 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2620 ms, Memory: 402912 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+
+def find_parent(x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+
+def union_parent(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+sys.setrecursionlimit(10**5)
+
+v, e = map(int, input().split())
+
+l = []
+parent = [x for x in range(v+1)]
+result = 0
+
+for i in range(e):
+    a, b, c = map(int, input().split())
+    l.append((a, b, c))
+
+l = sorted(l, key=lambda x: x[2])
+
+last = 0
+
+for edge in l:
+    a, b, c = edge
+    if find_parent(a) == find_parent(b):
+        continue
+    else:
+        last = c
+        result += c
+        union_parent(a, b)
+
+print(result - last)
