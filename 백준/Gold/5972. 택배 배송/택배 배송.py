a0b21777eebb37e90a22e9bb72d5d7d06218f9df
hanmo2000110 <74346222+hanmo2000110@users.noreply.github.com>
2024-03-29 16:40:58 +0900

    [Gold V] Title: 택배 배송, Time: 332 ms, Memory: 127880 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.py
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -1,38 +1,31 @@
-from sys import stdin as s
+import sys
 import heapq
-#제출 시 주석 필수
 
+n, m = map(int, sys.stdin.readline().rstrip().split())
+INF = sys.maxsize
+nodes = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b, c = map(int, sys.stdin.readline().rstrip().split())
+    nodes[a].append([b, c])
+    nodes[b].append([a, c])
 
-[house_num, load_num] = list(map(int, input().split(' ')))
-input_arr=[];
-graph=[[] for _ in range(house_num+1)]
-    
-for i in range(load_num):
-   [start, end, w] = list(map(int, s.readline().split(' ')));
-   #양방향 그래프 생성
-   graph[start].append([w, end]);
-   graph[end].append([w, start]);
+def Dijkstra(start):
+    distances = [INF for _ in range(n+1)]
+    distances[start] = 0
+    pq = []
+    heapq.heappush(pq, [0, start])
 
-INF=1e9;
-#방문체크 배열 (INF로 초기화)
-visited=[INF] * (house_num+1); 
-visited[1] = 0; # 시작점 1은 초기화
+    while pq:
+        cur_cost, cur_node = heapq.heappop(pq)
 
-q=[];
+        if distances[cur_node] < cur_cost: continue
 
-#시작 (정점 1)
-heapq.heappush(q, [0,1])
-while(q):
-    [w, v] = heapq.heappop(q);
-    #visited 배열의 기존값과 비교
-    if (visited[v] < w):
-        #볼 필요 없음
-        continue; 
-    for connected_w, connected_v in graph[v]:
-        if (connected_w + w < visited[connected_v]):
-            # 갱신 필요
-            visited[connected_v] = connected_w + w ;
-            # 이 점으로 뻗어나가야함
-            heapq.heappush(q, [visited[connected_v], connected_v])
-            
-print(visited[house_num]);
+        for next_node, next_cost in nodes[cur_node]:
+            if distances[next_node] > next_cost + cur_cost:
+                distances[next_node] = next_cost + cur_cost
+                heapq.heappush(pq, [next_cost + cur_cost, next_node])
+
+    return distances
+
+distances = Dijkstra(1)
+print(distances[n])
