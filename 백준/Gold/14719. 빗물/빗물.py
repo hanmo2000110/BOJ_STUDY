fd336eb009b9462c2c5d2f76874c411cd3f7d876
hanmo2000110 <74346222+hanmo2000110@users.noreply.github.com>
2025-02-06 09:47:16 +0900

    [Gold V] Title: 빗물, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,23 +1,21 @@
-H,W = map(int, input().split())
-limit = list(map(int, input().split())) 
-arr = [[] for _ in range(H)]
+H, W = map(int, input().split())
+limit = list(map(int, input().split()))
+
 result = 0
 
-for i in range(H):
-  for j in range(W):
-    if i < limit[j] :
-      arr[i].append(1)
-    else :
-      arr[i].append(0)
+# 각 위치에서 좌우의 최대 높이를 구하여 빗물이 고이는 양을 계산
+left_max = [0] * W
+right_max = [0] * W
 
-for i in range(H):
-  flag = 0
-  for j in range(1,W):
-    if arr[i][j] == 0 and arr[i][j-1] == 1 :
-      flag = j
-    elif arr[i][j] == 1 and flag != 0:
-      result += j - flag 
-      flag = 0
+left_max[0] = limit[0]
+for i in range(1, W):
+    left_max[i] = max(left_max[i - 1], limit[i])
 
+right_max[W - 1] = limit[W - 1]
+for i in range(W - 2, -1, -1):
+    right_max[i] = max(right_max[i + 1], limit[i])
+
+for i in range(W):
+    result += max(0, min(left_max[i], right_max[i]) - limit[i])
 
 print(result)
