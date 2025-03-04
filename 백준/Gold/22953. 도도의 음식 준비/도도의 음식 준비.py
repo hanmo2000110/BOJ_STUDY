258a69c7de8cb37424eb96b14e1cf0bbc2f43d1f
hanmo2000110 <74346222+hanmo2000110@users.noreply.github.com>
2025-03-04 21:48:31 +0900

    [Gold IV] Title: 도도의 음식 준비, Time: 132 ms, Memory: 110888 KB -BaekjoonHub

---
+++ 백준/Gold/22953. 도도의 음식 준비/도도의 음식 준비.py
@@ -0,0 +1,37 @@
+import sys
+
+input=sys.stdin.readline
+
+n,k,c=map(int,input().split())
+a=list(map(int,input().split()))
+s=sum(a)-len(a)
+
+def comb(idx, dep):
+  global a, k, s
+
+  if dep==c or dep>=s:
+    left=0
+    right=10**12
+    while left+1<right:
+      mid=(left+right)//2
+      cnt=0
+
+      for x in a:
+        cnt+=mid//x
+
+      if cnt>=k: right=mid
+      else: left=mid
+
+    return right
+
+  ans=10**12
+  for i in range(idx, len(a)):
+    if a[i]==1:continue
+
+    a[i]-=1
+    ans=min(ans,comb(i, dep+1))
+    a[i]+=1
+
+  return ans
+
+print(comb(0, 0))
