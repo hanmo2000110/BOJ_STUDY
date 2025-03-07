1482fdb64dd9ca39da0eb42e8a85d4415e89db05
hanmo2000110 <74346222+hanmo2000110@users.noreply.github.com>
2025-03-07 21:34:40 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 128 ms, Memory: 117360 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -1,14 +1,15 @@
-import sys
-input = sys.stdin.readline
 
-min_val, max_val = map(int, input().split())
-check = [True] * (max_val - min_val + 1)
+min,max = map(int,input().split())
+check = [True] * (max-min+1)
+result = 0
 
-for i in range(2, int(max_val**0.5) + 1):
-    square = i * i
-    start = ((min_val + square - 1) // square) * square  # min 이상에서 가장 가까운 제곱수 배수 찾기
-    
-    for j in range(start, max_val + 1, square):
-        check[j - min_val] = False  # 해당 수를 제거
+for i in range(2, int(max**0.5) + 1) :
+    temp = ((min + i**2 - 1) // i**2) * i**2
 
-print(sum(check))  # 남아있는 True 개수 출력
+    while max >= temp :
+        if temp >= min and check[temp-min]:
+            check[temp-min] = False
+            result += 1
+        temp += i**2
+
+print((max-min+1) - result)
