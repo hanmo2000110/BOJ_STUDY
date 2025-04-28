00eb76bc89f6dd357bf9d664fd8b19f4a2f69da9
hanmo2000110 <74346222+hanmo2000110@users.noreply.github.com>
2025-04-28 22:01:55 +0900

    [Platinum V] Title: 찾기, Time: 208 ms, Memory: 148312 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.py
+++ 백준/Platinum/1786. 찾기/찾기.py
@@ -2,7 +2,7 @@ def make_table(pattern):
     m = len(pattern)
     table = [0] * m
     j = 0  # 일치하는 접두사 길이
-    
+
     for i in range(1, m):
         while j > 0 and pattern[i] != pattern[j]:
             j = table[j - 1]
@@ -15,33 +15,30 @@ def KMP_search(text, pattern):
     n, m = len(text), len(pattern)
     table = make_table(pattern)
     result = []
-    
+
     i = 0  # text의 인덱스
     j = 0  # pattern의 인덱스
-    
+
     while i < n:
         if text[i] == pattern[j]:
             i += 1
             j += 1
             if j == m:
-                result.append(i - m + 1)  # 1-based index
+                result.append(i - j + 1)
                 j = table[j - 1]
         else:
             if j > 0:
                 j = table[j - 1]
             else:
                 i += 1
-    
+
     return result
 
-# 입력 처리
-T = input()  # 텍스트
-P = input()  # 패턴
+# 예시
+text = input()
+pattern = input()
 
-# KMP 알고리즘을 통해 찾기
-positions = KMP_search(T, P)
-
-# 결과 출력
-print(len(positions))  # 패턴이 등장한 횟수 출력
-if positions:
-    print(*positions)  # 패턴이 등장한 위치 출력
+ans = KMP_search(text, pattern)
+print(len(ans))  # [0, 10, 15]
+if ans:
+    print(*ans)
